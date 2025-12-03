ec6f266fc6efbc003c9853960ce291ccbc0ed5cc
mingdodev <mingdo.dev@gmail.com>
2025-12-03 20:24:59 +0900

    feat: 로그인 시 jwt 파싱으로 userRole 가져오기

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,3 +23,18 @@ export function formatDateTime(dateString: string): string {
     return dateString;
   }
 }
+
+export function parseJwt(token: string) {
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  } catch (error) {
+    console.error("Invalid token:", error);
+    return null;
+  }
+}
